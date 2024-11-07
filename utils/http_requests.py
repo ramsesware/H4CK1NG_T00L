bf56851fa447fcec65953e42c144252cb2313496
Moisés Ceñera Fernández <110106661+ramsesware@users.noreply.github.com>
2024-11-07 11:30:25 +0100

    Update http_requests.py

--- utils/http_requests.py
+++ utils/http_requests.py
@@ -1,3 +1,19 @@
+# Copyright (C) 2024 Moisés Ceñera Fernández
+# This file is part of H4CK1NG_T00L.
+# 
+# H4CK1NG_T00L is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# H4CK1NG_T00L is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with H4CK1NG_T00L. If not, see <https://www.gnu.org/licenses/>.
+
 import requests
 
 def make_request(url):
